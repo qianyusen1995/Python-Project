f2e4028d08cbfe5dd65ce2b7b33fab93b61417c7
Jefferson <1224817019@qq.com>
2022-07-25 14:43:16 +0800

    Django pizzas

--- Project3_WebApplication/learning_log/learning_logs/models.py
+++ Project3_WebApplication/learning_log/learning_logs/models.py
@@ -10,16 +10,16 @@ class Topic(models.Model): #继承了Model: Django中一个定义了模型基本
 
 class Entry(models.Model): #Entry 也继承了Django基类Model
     """学到的有关某个主题的具体知识"""
-    topic = models.ForeignKey(Topic,on_delete=models.CASCADE) #属性topic 是一个ForeignKey的实例，将条目与主题关联起来 
-    ##django 升级到2.O 以后，表之间的关联，必现写上on_delete 参数
-    text = models.TextField() # 属性text是一个TextField实例
-    date_added = models.DateTimeField(auto_now_add=True) # 属性date_added 让我们能够按创建顺序呈现条目，并在每个条目旁边放置时间戳。
-    class Meta: #Meta 类 存储用于管理模型的额外信息
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE) #属性topic是一个ForeignKey的实例，将条目与主题关联起来 
+    ##django 升级到2.O 以后，表之间的关联，必现写上on_delete参数
+    text = models.TextField() #属性text是一个TextField实例。这种字段不需要长度限制，因为我们不想限制条目的长度
+    date_added = models.DateTimeField(auto_now_add=True) #属性date_added 让我们能够按创建顺序呈现条目，并在每个条目旁边放置时间戳。
+    class Meta: #Meta类->存储用于管理模型的额外信息
         verbose_name_plural = 'entries'
     def __str__(self): #方法__str__() 告诉Django，呈现条目时应显示哪些信息
         """返回模型的字符串表示"""
         if len(self.text) > 50:
-            return self.text[:50] + "..." #只显示text 的前50个字符（见❺）。我们还添加了一个省略号，指出显示的并非整个条目。
+            return self.text[:50] + "..." #只显示text 的前50个字符。我们还添加了一个省略号，指出显示的并非整个条目。
         else:
             return self.text
     
---
+++ Project3_WebApplication/pizzeria/pizzas/models.py
@@ -0,0 +1,18 @@
+from pyexpat import model
+from django.db import models
+
+# Create your models here.
+# 定义一个名为Pizza 的模型，它包含字段name，用于存储比萨名称，如Hawaiian和Meat Lovers。
+# 定义一个名为Topping 的模型，它包含字段pizza 和name，其中字段pizza 是一个关联到Pizza 的外键，
+# 而字段name 用于存储配料，如pineapple、Canadian bacon 和sausage
+class Pizza(models.Model):
+    name = models.CharField(max_length=20)
+    date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        """返回模型的字符串表示"""
+        return self.name
+        
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
